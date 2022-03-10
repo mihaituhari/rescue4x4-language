ed296f6ef4fe10d5a74f1cb3a85691b019516b1e
mihai <mihai.tuhari@gmail.com>
2022-03-10 23:34:35 +0200

    language: add RO, EN, UA

---
+++ src/language_en.inc.php
@@ -0,0 +1,276 @@
+<?php
+
+$ARR_LANGUAGE_EN = [
+    // Global
+    'brand' => 'Rescue 4x4',
+    'phone' => '0377779000',
+    'phone_human' => '0377 779 000',
+
+    // Homepage
+    'homepage.title' => 'Request help',
+    'homepage.content' => 'If you got stuck or you need the help of Rescue4x4 community, click the button below.
+        It is very important that you provide accurate information.<br><br>
+        In the moment you will report an incident, this will be evaluated and the members around you will be notified.<br><br>
+        Rescue4x4 is a <b>free</b> community volunteering service.<br><br>
+        <b>If you are in an emergeny situation, call 112!</b>',
+    'homepage.button' => 'Request help now',
+
+    // Header
+    'header.member-unavailable' => 'You are marked as <b>unavailable</b> and you don\'t reiceve alerts!',
+
+    // Auth
+    'auth.sign-out' => 'Logout',
+    'auth.sign-up' => 'Signup',
+    'auth.sign-in' => 'Signin',
+
+    // Breadcrumbs
+    'breadcrumbs.home' => 'Home',
+    'breadcrumbs.community' => 'Community',
+    'breadcrumbs.incidents-history' => 'Incidents history',
+
+    // Footer
+    'footer.cookie-policy' => 'Cookie policy',
+    'footer.cookie-alert-button' => 'I agree',
+    'footer.cookie-alert-content' => 'This site uses cookies to offer you the best experience. <a href="/politica-cookie">Find out more.</a>',
+
+    // Help page
+    'help.terms.title' => 'Confirm terms and conditions',
+    'help.info.field-placeholder' => 'Offer as many details about the situation, landscape, meteo conditions, etc',
+    'help.sending-text' => 'Processing request, do not close the page!',
+    'help.photos.instructions' => 'Add photos from the location, so the incident is evaluated correctly by the team.<br>' .
+        '<small>Avoid photos with license plates & faces.</small>',
+
+    'help.disclaimer.free-service' => '
+        <ul>
+             <li>Rescue 4x4 is a <b>FREE</b> community public service! Please notify us of
+                 as soon as a volunteer, a Rescue4x4 member, asks you for money.
+            </li>
+             <li>Rescue 4x4 is <b>NOT</b> a platform for towing vehicles or vehicle transport service.
+                 Rescue4x4 is also <b>NOT</b> a technical vehicle repair service.
+            </li>
+             <li>The free <b>service</b> offered by Rescue4x4 is to bring your vehicle to the road
+                 (paved or not) from where you will be able to go on your own wheels or from
+                 removing the vehicle from the carriageway of a public road so as not to disturb it
+                 or block traffic.
+            </li>
+        </ul>',
+
+    'help.disclaimer.terms' => '
+        <strong class="red">Prin bifarea căsuței alăturare sunt de acord cu condițiile platformei Rescue 4x4, și anume că:</strong>
+        <ul>
+            <li>Am nevoie de asistență din partea comunității Rescue 4x4 și <strong>nu sunt într-o situație critică</strong> ce impune apelarea numărului unic de urgență <strong>112</strong>.</li>
+            <li>Solicitarea mea poate fi <strong>respinsă</strong> dacă nu este de domeniul de activitate al Rescue 4x4.</li>
+            <li>Sunt de acord cu <strong>exonerarea de răspunderea materială</strong> a voluntarilor pentru posibilele daune produse pe timpul acţiunilor de intervenţie și recuperare executate.</li>
+            <li>Sunt de acord ca <strong>datele introduse</strong> de mine mai sus, respectiv nume, număr de telefon și localizare să fie public disponibile membrilor Rescue 4x4.</li>
+        </ul>',
+
+    // Incident details/submitted related texts
+    'incident.approximate-map-location' => 'Map location is not accurate',
+    'incident.submitted-alert-scams' => 'If you are requested to offer money or goods, <strong>refuse</strong> strongly and call urgently <a href="tel:0377779000">0377 779 000</a> !',
+    'incident.requester-details' => 'Requester details',
+    'incident.intervention-title' => 'Intervention',
+    'incident.notified-members' => 'notified members',
+    'incident.attending' => 'members attending',
+    'incident.confirm-resolved' => 'confirms resolved',
+    'incident.button-attend-intervention' => 'I attend',
+    'incident.button-incident-resolved' => 'Incident resolved',
+
+    // Form fields
+    'form.help.location' => 'City / District',
+    'form.captcha' => 'Verification code',
+
+    // Common
+    'common.emergency-phone' => 'Emergency phone',
+    'common.emergency' => 'Emergencies',
+    'common.contact-details' => 'Contact details',
+    'common.request-help' => 'Request help',
+    'common.optional' => 'optional',
+    'common.photos' => 'Photos',
+    'common.button-send' => 'Send',
+    'common.share' => 'Share',
+    'common.open' => 'Open',
+    'common.landmark' => 'Landmark',
+    'common.unspecified' => 'unspecified',
+    'common.gps-coordinates' => 'GPS coordinates',
+    'common.gps' => 'GPS',
+    'common.members' => 'members',
+    'common.see-members' => 'see members',
+    'common.members-list' => 'members list',
+    'common.location' => 'Localization',
+    'common.locality' => 'Locality',
+    'common.coverage' => 'Coverage',
+    'common.availability' => 'Availability',
+    'common.reported-incidents' => 'Incidents reported',
+    'common.member-since' => 'Member since',
+    'common.asv4x4-code' => 'ASV4x4 code',
+    'common.county' => 'County',
+    'common.address' => 'Address',
+    'common.postal-code' => 'Postcode',
+    'common.latitude' => 'Latitude',
+    'common.longitude' => 'Longitude',
+    'common.firstname' => 'Firstname',
+    'common.lastname' => 'Lastname',
+    'common.name' => 'Name',
+    'common.phone' => 'Phone',
+    'common.mobile' => 'Mobile',
+    'common.mobile-secondary' => 'Secondary mobile',
+    'common.information' => 'Information',
+    'common.coordination' => 'Coordination',
+    'common.coordination-channel' => 'Coordination channels',
+    'common.menu' => 'Menu',
+    'common.change-here' => 'Change here',
+    'common.incidents' => 'Incidents',
+    'common.details' => 'Details',
+    'common.see-details' => 'See details',
+    'common.next-page' => 'Next',
+    'common.previous-page' => 'Previous',
+    'common.incident-details' => 'Incident details',
+    'common.email' => 'Email',
+    'common.password' => 'Password',
+    'common.from' => 'from',
+    'common.back-to' => 'Back to',
+    'common.back-to-list' => 'Back to list',
+    'common.error' => 'Error',
+    'common.form-successfully-sent' => 'Form successfully sent.',
+    'common.message' => 'message',
+    'common.messages' => 'messages',
+    'common.national' => 'national',
+    'common.save' => 'Save',
+    'common.info' => 'Info',
+    'common.alerts' => 'Alerts',
+    'common.interventions' => 'Interventions',
+    'common.show-phone-number' => 'Show number',
+    'common.terms-and-conditions' => 'Terms and conditions',
+    'common.profile' => 'Profile',
+    'common.available' => 'Available',
+    'common.unavailable' => 'Unavailable',
+    'common.yes' => 'Yes',
+    'common.no' => 'No',
+    'common.reason' => 'Reason',
+
+    // Account
+    'account.back-to-login' => 'Back to login',
+    'account.password-reset' => 'Reset password',
+    'account.password-reset-success' => 'An email with password reset instructions was sent.',
+    'account.current-password' => 'Current password',
+    'account.new-password' => 'New password',
+    'account.change-password' => 'Change password',
+    'account.cancel-change-password' => 'Cancel password change',
+    'account.repeat-password' => 'Repeat password',
+    'account.no-new-messages' => 'No new messages',
+    'account.address-is-optional' => 'Address is optional',
+    'account.notifications-whole-county' => 'Whole country',
+    'account.notifications-km-surface' => 'KM surface',
+    'account.notifications-km-number' => 'KM number',
+    'account.how-can-i-help' => 'How I can help',
+    'account.setup-info' => 'Info about car, equipment, accessories and how I can help.',
+    'account.new-account' => 'New account',
+    'account.incidents-history' => 'History on incidents interventions',
+    'account.incidents-history-none' => 'No intervention so far',
+    'account.alerts-received' => 'Received alerts',
+    'account.chat-sound-enabled' => 'Chat sound - Enabled',
+    'account.chat-sound-disabled' => 'Chat sound - Disabled',
+    'account.success-updated' => 'Account successfully updated',
+    'account.success-activated' => 'Account successfully activated',
+    'account.asv4x4-code-details' => 'This is your unique code which you can use during the interventions.',
+
+    // Login
+    'login.title' => 'Signin',
+    'login.members-only' => 'The page you are trying to access is visible only for the logged in members.',
+
+    // Signup
+    'signup.disclaimer.terms' => '
+        <strong class="red">By checking the checkbox next to this I agree with the terms of the Rescue 4x4 platform, namely that:</strong>
+        <ul>
+            <li>I agree that the <strong>data</strong> I provide (such as first name, last name, address, phone number, email address, location, but not only) should be available to the <strong>coordinators</strong> Rescue 4x4.</li>
+            <li>I agree that <strong>my phone number and name</strong> should be available to <strong>Rescue 4x4 members notified in the event of an incident in which I am also associated, for further easier communication.</li>
+            <li>I agree that I will intervene in the resolution of incidents at my own risk and within the limits of my capabilities and <strong>I will have no claims</strong> from Rescue 4x4.</li>
+            <li><strong>Note:</strong> Rescue 4x4 <strong>will not</strong> provide your data for commercial purposes to any third party.</li>
+        </ul>',
+
+    // Profile
+    'profile.title' => 'My account',
+    'profile.account-details' => 'Account details',
+    'profile.asv4x4' => 'ASV4x4 info',
+    'profile.info-emergency' => 'Emergency situations info',
+    'profile.documents' => 'Documents and procedures',
+    'profile.alert-channels' => 'Alert channels',
+    'profile.my-public-profile' => 'My public profile',
+    'profile.delete-account' => 'Delete account',
+    'profile.account-details-hint' => 'Your account access details',
+    'profile.contact-details-hint' => 'Public information for people who need your help.',
+    'profile.location-details-hint' => 'Your location. used for determining when you could help. After selecting the county, start typing the locality and click from the list.',
+    'profile.alerts-settings' => 'Alerts and notifications settings',
+    'profile.notifications-details-hint' => 'Settings for alert notifications. Define a radius of KM around the locality you live in (min 15km , max 250km) or select the entire county.',
+    'profile.available-details-hint' => 'In case you are not available to help for some time, change the option below. While you are iunavailable you will not receive alerts.',
+
+    // Delete account
+    'delete-account.title' => 'I want to have my Rescue 4x4 member account deleted',
+    'delete-account.reason-placeholder' => 'Here you can write the reason which made you delete the account...',
+    'delete-account.button' => 'Delete account',
+    'delete-account.success-message' => 'Account successfully deleted.',
+    'delete-account.info' => '
+        <p>You are about to delete your Rescue 4x4 member account.</p>
+        <p><b>This operation cannot be undone!</b></p>
+        <ul>
+            <li>You will be deleted from the database.</li>
+            <li>You will stop receiving alerts (SMS/email).</li>
+            <li>You will stop receiving emails from us.</li>
+            <li>You will be able to signup again at any time.</li>
+        </ul>',
+
+    // Menu navigation
+    'menu.about' => 'About',
+    'menu.cookie-policy' => 'Cookie policy',
+    'menu.stickers' => 'Stickers',
+    'menu.community' => 'Community',
+    'menu.members-map' => 'Members map',
+    'menu.facebook-group' => 'Facebook group',
+    'menu.equipment' => 'Equipment',
+    'menu.alert-channels' => 'Alert channels',
+    'menu.partners' => 'Partners',
+    'menu.contact' => 'Contact',
+
+    // Validation & errors
+    'validation.form-errors-title' => 'Please fix the errors from the form.',
+    'validation.invalid-phone' => 'Invalid phone number',
+    'validation.invalid-email' => 'Invalid email address',
+    'validation.confirm-to-proceed' => 'Please confirm in order to proceed',
+    'validation.required-field' => 'Mandatory field',
+    'validation.incorrect-signature' => 'Incorrect signature',
+    'validation.incorrect-password' => 'Incorrect password',
+    'validation.incorrect-email' => 'Incorrect email',
+    'validation.invalid-recaptha' => 'Incorrect captcha',
+    'validation.repeat-password' => 'Repeat password',
+    'validation.phone-used' => 'Phone already used',
+    'validation.email-used' => 'Email already used',
+    'validation.confirm-email' => 'Confirm email address',
+    'validation.confirm-password' => 'Confirm password',
+    'validation.use-different-phone-number' => 'Use a different phone from the main one',
+    'validation.tick-at-least-one-option' => 'Tick at least one option',
+    'validation.city-autocomplete-hint' => 'Start typing and then choose a city from the list, by clicking it',
+    'validation.fill-all-fields' => 'Fill all the form fields',
+    'validation.repeat-password-not-matching' => 'Password and repeated password do not match',
+    'validation.password-length' => 'The password must have at least :characters characters',
+    'validation.incorrect-credentials' => 'Incorrect credentials',
+
+    // Errors
+    'error.generic' => 'An error occured',
+    'error.account-inactive' => 'Your account is not active yet. Check your inbox for the activation email (including SPAM folder) or <a href="/contact">contact us</a> if you encounter issues',
+    'error.invalid-reset-link' => 'Invalid password reset link',
+
+    // Contact/enquiries page
+    'enquiries.equipment-info' => 'Please call use if you have an emergency or you wnat to report an incident.<br><br>
+        For questions about <b>equipment order status</b> please reply to the email that you received after you placed the order.',
+
+    // Chat box
+    'chat.title' => 'Rescue4x4 community chat',
+    'chat.load-older-messages' => 'Load older messages',
+    'chat.type-here' => 'Type your message here ...',
+
+    // Form success
+    'success.message-sent' => 'Message successfully sent.',
+
+    // Static pages
+    'static.404-not-found' => 'Page not found',
+];
